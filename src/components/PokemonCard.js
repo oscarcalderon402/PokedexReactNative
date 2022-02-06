aec22adce259900f48cb747213f3cc76808d0ad2
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2022-02-06 04:06:09 -0400

    clasification by type

--- src/components/PokemonCard.js
+++ src/components/PokemonCard.js
@@ -7,23 +7,29 @@ import {
 } from 'react-native';
 
 import React from 'react';
+import { capitalize } from 'lodash';
+import getColorByPokemonType from '../utils/getColorByPokemonType';
 
 export default function PokemonCard(props) {
   const { pokemon } = props;
 
+  const pokemonColor = getColorByPokemonType(pokemon.type);
+  const bgStyles = { backgroundColor: pokemonColor, ...styles.bgStyles };
+  console.log(pokemonColor);
   const goToPokemon = () => {
     console.log(`Vamos al pokemon: ${pokemon.name}`);
     console.log(pokemon);
   };
+
   return (
     <TouchableWithoutFeedback onPress={goToPokemon}>
       <View style={styles.card}>
         <View style={styles.spacing}>
-          <View style={styles.bgStyles}>
+          <View style={bgStyles}>
             <Text style={styles.number}>
               #{`${pokemon.order}`.padStart(3, 0)}
             </Text>
-            <Text style={styles.name}>{pokemon.name}</Text>
+            <Text style={styles.name}>{capitalize(pokemon.name)}</Text>
             <Image source={{ uri: pokemon.image }} style={styles.image} />
           </View>
         </View>
@@ -42,7 +48,9 @@ const styles = StyleSheet.create({
     padding: 5,
   },
   bgStyles: {
-    backgroundColor: 'grey',
+    flex: 1,
+    borderRadius: 15,
+    padding: 10,
   },
   number: {
     position: 'absolute',
@@ -55,7 +63,6 @@ const styles = StyleSheet.create({
     color: '#fff',
     fontWeight: 'bold',
     fontSize: 15,
-    paddingTop: 10,
   },
   image: {
     position: 'absolute',
