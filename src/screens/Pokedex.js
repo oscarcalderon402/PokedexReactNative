501b57c8a75184e5047878c79870bb30646bf929
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2022-02-06 00:56:25 -0400

    get info

--- src/screens/Pokedex.js
+++ src/screens/Pokedex.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { getPokemonApi, getPokemonDetailsByUrlApi } from '../api/pokemon';
 
 export default function Pokedex() {
+  const [pokemons, setPokemons] = useState([]);
+  console.log(pokemons);
+
+  useEffect(() => {
+    (async () => {
+      await loadPokemons();
+    })();
+  }, []);
+
+  const loadPokemons = async () => {
+    try {
+      const response = await getPokemonApi();
+      const pokemonsArray = [];
+      for await (const pokemon of response.results) {
+        const pokemonDetails = await getPokemonDetailsByUrlApi(pokemon.url);
+        pokemonsArray.push({
+          id: pokemonDetails.id,
+          name: pokemonDetails.name,
+          type: pokemonDetails.types[0].type.name,
+          order: pokemonDetails.order,
+          imagen:
+            pokemonDetails.sprites.other['official-artwork'].front_default,
+        });
+      }
+
+      setPokemons([...pokemons, ...pokemonsArray]);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   return (
     <SafeAreaView>
       <Text>Pokedex</Text>
