898ca3306d411203981d60aa1325bd2d5b5a7310
Oscar Calderon <gregoriocalderon.07@hotmail.es>
2022-02-19 18:19:25 -0400

    formik and yup

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -1,7 +1,18 @@
 import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
 import React from 'react';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
 
 export default function LoginForm() {
+  const formik = useFormik({
+    initialValues,
+    validationSchema: Yup.object(validationSchema),
+    validateOnChange: false,
+    onSubmit: (formValue) => {
+      console.log('formulario enviando');
+      console.log(formValue);
+    },
+  });
   return (
     <View style={styles.container}>
       <Text style={styles.title}>Iniciar sesión</Text>
@@ -9,24 +20,40 @@ export default function LoginForm() {
         placeholder="Nombre de usuario"
         style={styles.input}
         autoCapitalize="none"
+        value={formik.values.username}
+        onChangeText={(text) => formik.setFieldValue('username', text)}
       />
       <TextInput
         placeholder="Contraseña"
         style={styles.input}
         autoCapitalize="none"
         secureTextEntry={true}
+        value={formik.values.password}
+        onChangeText={(text) => formik.setFieldValue('password', text)}
       />
       <View style={styles.button}>
-        <Button title="Entrar" onPress={() => console.log('Entrando...')} />
+        <Button title="Entrar" onPress={formik.handleSubmit} />
       </View>
+      <Text style={styles.error}>{formik.errors.username}</Text>
+      <Text style={styles.error}>{formik.errors.password}</Text>
     </View>
   );
 }
 
+const initialValues = {
+  username: '',
+  password: '',
+};
+
+const validationSchema = {
+  username: Yup.string().required('El usuario es obligatorio'),
+  password: Yup.string().required('La contrasena es obligatoria'),
+};
+
 const styles = StyleSheet.create({
-  container: {
-    top: '30%',
-  },
+  //   container: {
+  //     top: '30%',
+  //   },
   title: {
     textAlign: 'center',
     fontSize: 28,
@@ -45,5 +72,10 @@ const styles = StyleSheet.create({
     width: '95%',
     alignSelf: 'center',
     // top: 100,
+  },
+  error: {
+    textAlign: 'center',
+    color: '#f00',
+    marginTop: 20,
   },
 });
